2d3e3b76cb98dfcda750a1e2e5e8decc3e740be2
AnanthPrabha <79782270+AnanthPrabha@users.noreply.github.com>
2021-09-03 10:22:42 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,12 @@
 
-resource "aws_instance" "testec2" {
+resource "aws_instance" "mytask" {
     ami = "ami-0f511ead81ccde020"
     instance_type = "t2.micro"
     key_name = "Sing"
     tags = {
         Name = "mytest"
     }
+  provisioner "local-exec" {
+    command = "echo ${aws_instance.mytask.public_ip} > inventory.txt"
+}
 }
