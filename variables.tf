770542ea99bfb468b87ccbb98252125aea91d807
AnanthPrabha <79782270+AnanthPrabha@users.noreply.github.com>
2021-09-04 01:31:48 +0530

    Add files via upload

---
+++ variables.tf
@@ -0,0 +1,30 @@
+variable "aws_region" {
+    default = "ap-southeast-1"
+}
+variable "instance_type" {
+    default = "t2.micro"
+}
+variable "instance_name" {
+    default = "terra-ansible"
+}
+variable "ami_id" {
+    default = "ami-082105f875acab993"
+}
+variable "ssh_user_name" {
+    default = "ec2-user"
+}
+variable "ssh_key_name" {
+    default = "Sing"
+}
+variable "ssh_key_path" {
+    default = "~/.ssh/Sing.pem"
+}
+variable "instance_count" {
+    default = 1
+}
+variable "subnet_id" {
+    default = "subnet-e03d78ce"
+}
+variable "dev_host_label" {
+    default = "terra_ansible_host"
+}
