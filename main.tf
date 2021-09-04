cc1851208560f9f60419df4f0f9c7ae64d4086e8
AnanthPrabha <79782270+AnanthPrabha@users.noreply.github.com>
2021-09-04 08:26:46 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "myInstanceAWS" {
   count = "${var.instance_count}"
   instance_type = "${var.instance_type}"
   ami = "${var.ami_id}"
-  key_name = "Sing"
+  key_name = "Sing.pem"
 }
 
 resource "null_resource" "ConfigureAnsibleLabelVariable" {
