96eae1b54befdbf3738822f04ec15898cfa2a1d3
AnanthPrabha <79782270+AnanthPrabha@users.noreply.github.com>
2021-09-04 08:54:53 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -17,7 +17,7 @@ variable "ssh_user_name" {
  #   default = "Sing."
 #}
 variable "ssh_key_path" {
-   default = "/root/mytask/Sing.pem"
+   default = "./Sing.pem"
 }
 variable "instance_count" {
     default = 1
