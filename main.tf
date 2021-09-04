c7e70dcb69d7684b88199e57afae9981fafd7da6
AnanthPrabha <79782270+AnanthPrabha@users.noreply.github.com>
2021-09-04 10:48:16 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -33,6 +33,13 @@ resource "null_resource" "ProvisionRemoteHostsIpToAnsibleHosts" {
     host = "${element(aws_instance.myInstanceAWS.*.public_ip, count.index)}"
     private_key = "${file("${var.ssh_key_path}")}"
   }
+   provisioner "remote-exec" {
+    inline = [
+      "sudo yum update -y",
+      "sudo yum install python-setuptools python-pip -y",
+      "sudo pip install httplib2"
+      ]
+   }
 
   provisioner "local-exec" {
     command = "echo ${element(aws_instance.myInstanceAWS.*.public_ip, count.index)} >> hosts"
